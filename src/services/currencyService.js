743783e0d639af0a6656546a7bac4b90f9d0b3f7
SherifHodHod <sherif962003@gmail.com>
2024-06-27 17:31:20 +0300

    added news api

--- src/services/currencyService.js
+++ src/services/currencyService.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
-const URL = "https://api.exchangeratesapi.io/latest?base=USD";
+const API_KEY = "cur_live_UiEiheEvg133MszjQugoByRexjOcMhLj40bZLUzX";
+const URL =
+  "https://api.currencyapi.com/v3/range?datetime_start=2021-11-30T23:59:59Z&datetime_end=2021-12-31T23:59:59Z&accuracy=day";
 
 export const fetchCurrencyRates = async () => {
   try {
-    const response = await axios.get(URL);
-    return response.data.rates.EGP;
+    const response = await axios.get(URL, {
+      headers: {
+        apikey: API_KEY,
+      },
+    });
+    return response.data;
   } catch (error) {
     console.error("Error fetching currency rates:", error);
     return null;
--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "newsapi.org";
+const API_KEY = "2befa5a94f3a4aa59d8368ca7a22c065";
 const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 
 export const fetchNews = async () => {
