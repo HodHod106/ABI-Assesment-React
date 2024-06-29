9ed1ee8def7f8108e2a301a54dfe964df6aa5646
SherifHodHod <sherif962003@gmail.com>
2024-06-29 15:32:30 +0300

    updating golding rates > added gold types

--- src/components/GoldPrices.jsx
+++ src/components/GoldPrices.jsx
@@ -13,12 +13,50 @@ const GoldPrices = () => {
   }, []);
 
   return (
-    <div className="grid-container">
+    <div className="table-container">
       {goldPrices && (
-        <div className="grid-item">
-          <h3>Gold Price (USD/Oz)</h3>
-          <p>{goldPrices.price}</p>
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Gold Type</th>
+              <th>Price (USD/gram)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>24K</td>
+              <td>{goldPrices.price_gram_24k}</td>
+            </tr>
+            <tr>
+              <td>22K</td>
+              <td>{goldPrices.price_gram_22k}</td>
+            </tr>
+            <tr>
+              <td>21K</td>
+              <td>{goldPrices.price_gram_21k}</td>
+            </tr>
+            <tr>
+              <td>20K</td>
+              <td>{goldPrices.price_gram_20k}</td>
+            </tr>
+            <tr>
+              <td>18K</td>
+              <td>{goldPrices.price_gram_18k}</td>
+            </tr>
+            <tr>
+              <td>16K</td>
+              <td>{goldPrices.price_gram_16k}</td>
+            </tr>
+            <tr>
+              <td>14K</td>
+              <td>{goldPrices.price_gram_14k}</td>
+            </tr>
+            <tr>
+              <td>10K</td>
+              <td>{goldPrices.price_gram_10k}</td>
+            </tr>
+          </tbody>
+        </table>
       )}
     </div>
   );
--- src/services/goldService.jsx
+++ src/services/goldService.jsx
@@ -1,18 +1,20 @@
 import axios from "axios";
 
-const API_KEY = "goldapi-2rg15slxx67d3a-io";
+const API_KEY = "goldapi-h4q1slxzxs3fs-io";
 
 const URL = "https://www.goldapi.io/api/XAU/USD";
 
 export const fetchGoldPrices = async () => {
   try {
     //console.log("Goldlll  ");
-    return null; //remove later
+    // return null; //remove later
     const response = await axios.get(URL, {
       headers: {
         "x-access-token": API_KEY,
       },
     });
+
+    // console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Error fetching gold prices:", error);
